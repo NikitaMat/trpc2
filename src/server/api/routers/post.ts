acd4c47801360c5dce141e469c2d4b526f60ec1f
nm <radot@LAPTOP-D8OJ6C9C>
2024-11-15 13:29:48 +0100

    user Router

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,8 +1,7 @@
 import { z } from "zod";
-
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
-export const postRouter = createTRPCRouter({
+export const userRouter = createTRPCRouter({
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
     .query(({ input }) => {
@@ -12,20 +11,28 @@ export const postRouter = createTRPCRouter({
     }),
 
   create: publicProcedure
-    .input(z.object({ name: z.string().min(1) }))
+    .input(z.object({ 
+      name: z.string().min(1), 
+      email : z.string().email("Bitte geben sie eine gültige E-Mail ein"),
+      age : z.number().int("Das Alter muss eine ganzzahl sein").min(12,"Das mindestalter ist 12")
+    }))
     .mutation(async ({ ctx, input }) => {
-      return ctx.db.post.create({
-        data: {
-          name: input.name,
-        },
-      });
+      return ctx.db.user.create(
+        {
+          data : {
+            name : input.name,
+            email : input.email,
+            age: input.age
+          }
+        }
+      );
     }),
 
   getLatest: publicProcedure.query(async ({ ctx }) => {
-    const post = await ctx.db.post.findFirst({
-      orderBy: { createdAt: "desc" },
+    const user = await ctx.db.user.findFirst({
+      orderBy: { name: "desc" },
     });
 
-    return post ?? null;
+    return user ?? null;
   }),
 });
